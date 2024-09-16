e340fba996fedb5e3aba1f52f92e2c6165f6d7cd
alankuruczBCE <alan.kurucz@bcestudent.com>
2024-09-16 01:10:39 +0100

    i dont know what i added that well
    i think i added the enemy chance spawning

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,6 @@
 ﻿
 from shutil import move
-import time, windowFunction
+import time, windowFunction, random
 
 
 
@@ -12,38 +12,69 @@ section = 1
 location = 1
 locationName = "Test"
 selection = 0
+randomEnemy = 0
 
-def areaLevel_(index):
+def areaLevel_(): #MAIN GAMEPLAY FUNC
     global section
     global location
     global locationName
-    global selection
+    global selection #MAKE VARIABLES GLOBAL
+
     windowFunction.ClearFrame_()
-    print("Level: " + str(level) + "            " + "XP - " + str(xp))
-    if section in range(0,6):
+    print("Level: " + str(level) + "            " + "XP - " + str(xp)) #PRINT STATS
+
+    if section in range(0,6): #CALCULATE SECTION
         location = 1
     elif section in range(6, 11):
         location = 2
-    if location == 1:
+
+    if location == 1: #FIND LOCATION NAME, SELECT INDEX FOR PRINTING
         locationName = "Granfield"
+        index = 0
     elif location == 2:
         locationName = "Granfield - Hills"
-    sectionReal = section - ((location-1) * 5)
-    print(locationName, "-", str(sectionReal))
-    windowFunction.locationArt(index)
-    print("1 - Search Location \n2 - View Inventory \n3 - Progress")
-    selection = int(input(""))
+        index = 0
+
+    sectionReal = section - ((location-1) * 5) #CALCULATE DISPLAY SECTION
+    print(locationName, "-", str(sectionReal)) #PRINT DISPLAY SECTION
+
+    windowFunction.locationArt(index) #PRINT LOCATION ART
+
+    print("1 - Search Location \n2 - View Inventory \n3 - Progress")#PRINT OPTIONS
+
+    selection = int(input("")) #SELECTION DETECTION CODE
     if selection == 1:
-        pass
+        searchArea_()
     elif selection == 2:
         pass
     elif selection == 3:
         section = section + 1
-        areaLevel_(0)
+        areaLevel_()
     else:
-        areaLevel_(0)
+        areaLevel_()
 
 
+
+def searchArea_():
+    global randomEnemy
+    global xp
+    
+    randomEnemy = random.randint(1,2)
+    if randomEnemy == 1:
+        print("Enemy found!")
+        xp = xp + 10
+        time.sleep(1)
+        areaLevel_()
+    else:
+        print("no enemy")
+        time.sleep(1)
+        areaLevel_()
+
+
+
+
+
+areaLevel_() #INITALISE MAIN GAMEPLAY MENU
 windowFunction.ClearFrame_()
 print("Loading", end='\r') #Print loading, do a carriage return
 time.sleep(0.3) #Pause for 0.3 seconds
@@ -60,12 +91,11 @@ name = input("                                                       ") #NAME PR
 windowFunction.Center_() #CENTER NAME PROMPT
 windowFunction.typeSlow_("Welcome to a perilous journey. There will be challenges and triumphs. You have seen nothing as of far.") #SLOW TYPE 1 LINE
 time.sleep(1)
-windowFunction.storySetup("Mladav Bolen - 1654", 0) #SHOW TEXT ON TOP OF IMAGE, PRINT IMAGE BELOW
+windowFunction.storySetup_("Mladav Bolen - 1654", 0) #SHOW TEXT ON TOP OF IMAGE, PRINT IMAGE BELOW
 windowFunction.Text_("you are surrounded by an air of desolateness. (ENTER to continue)")
 windowFunction.Text_("A faint rustle breaks the silence for a second.")
 windowFunction.Text_("Is it time..? I'm probably not going to come out walking but it's worth a try.")
 windowFunction.flashAnim_() #CHANGE SCENE WITH FLASHING ANIMATION
 windowFunction.Text_("Oh. What's this then... another one? Bloody hell.. they're like vermin.")
 windowFunction.Center_()
-
-areaLevel_(0) #INITALISE MAIN GAMEPLAY MENU
+areaLevel_() #INITALISE MAIN GAMEPLAY MENU
