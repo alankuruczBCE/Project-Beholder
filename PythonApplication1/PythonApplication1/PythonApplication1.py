35bd1005b82055029c7bced53dc12a34e4b134b8
alankuruczBCE <alan.kurucz@bcestudent.com>
2024-09-25 12:06:44 +0100

    CLEAN UP
    cleaned up my code lads

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,17 +1,7 @@
-﻿
-from ast import Constant
-from cgitb import text
-from operator import eq
-from pickle import APPEND
-from shutil import move
+﻿#-----------------------------------IMPORTS----------------------------------¦
 import time, windowFunction, random
-from urllib import response
-from tkinter.font import names
-from turtle import clear
-from tkinter import END
-from venv import create
 
-
+#-----------------------------------VARIABLES--------------------------------¦
 health = 50
 level = 1.0
 xp = 0
@@ -30,15 +20,20 @@ listHistory = []
 xpNeeded = 100
 inventory = [["Apple", 5],["Apple", 5]]
 apple = ["Apple", 5]
-slopList = ["john pork", "skibidi", "toilet", "soy", "wojak", "baby gronk", "friend", "kris", "senan", "roblox", "homestuck", "speed", "fanum", "brainrot", "camera", "mario", "chud", "chad", "costco", "among us", "sus", "mewing", "sigma", "hawk", "tuah", "haircut"]
+slopList = ["john pork", "skibidi", "toilet", "soy", "wojak", "baby gronk"
+            , "friend", "kris", "senan", "roblox", "homestuck", "speed", 
+            "fanum", "brainrot", "camera", "mario", "chud", "chad", 
+            "costco", "among us", "sus", "mewing", "sigma", "hawk", 
+            "tuah", "haircut"]#LIST TO STOP ENTRY OF BRAINROT NAMES
 slopFree = 0
 sectionPause = 0
 tick = 0
 levelLock = 0
 amountFights = 0
 
-
-def respond_(responseWords, responseText = "What do you respond with?    "):
+# FUNCTION DECLARATIONS
+def respond_(responseWords = "test", responseText = "What do you respond" 
+             "with?    "):
     global tick
     print()
     print()
@@ -62,10 +57,14 @@ class Weapon:
         self.active = active
 
     def summary(self):
-        print(self.index, ":", self.name, "  Damage: ", str(self.damage), "\nDescription:  ", self.description)
+        print(self.index, ":", self.name, "  Damage: ", str(self.damage), 
+              "\nDescription:  ", self.description)
 
-RustyBlade = Weapon(1,"Rusty Training Blade", 2000000, "It may or may not do the job. I don't think it will but it's up to you.", True)
-CrazyBlade = Weapon(2,"Basic Basher", 40, "A basic sword. It works somewhat well", False)
+RustyBlade = Weapon(1,"Rusty Training Blade", 2000000, 
+"It may or may not do the job. "
+"I don't think it will but it's up to you.", True)
+CrazyBlade = Weapon(2,"Basic Basher", 40, "A basic sword. "
+                    "It works somewhat well", False)
 equipped = RustyBlade
 power = equipped.damage
 class Enemy:
@@ -90,9 +89,10 @@ def areaLevel_(): #MAIN GAMEPLAY FUNC
     xpNeeded = ((50**(1 + (level * 0.05))))
 
     windowFunction.ClearFrame_()
-    print("Level: " + str(level) + "            " + "XP - " + str(xp) + "             " + "XP until level", str(int(level) + 1) + " -", str(round(xpNeeded) - xp)) #PRINT STATS
+    print("Level: " + str(level) + "            " + "XP - " + str(xp) + 
+          "             " + "XP until level", str(int(level) + 1)
+         + " -", str(round(xpNeeded) - xp)) #PRINT STATS
         
-
 
     if section in range(0,6): #CALCULATE SECTION
         location = 1
@@ -113,9 +113,12 @@ def areaLevel_(): #MAIN GAMEPLAY FUNC
 
     
     if level - 1 >= section:
-        print("1 - Fight \n2 - View Inventory \n3 - Progress \n4- Add an Apple")#PRINT OPTIONS
+        print("1 - Fight \n2 - View Inventory \n3 - Progress \n"
+              "4- Add an Apple")#PRINT OPTIONS
     else:
-        print("1 - Fight \n2 - View Inventory \n3 - (NEED TO LEVEL UP TO PROGRESS) \n4- Add an Apple")#PRINT OPTIONS
+        print("1 - Fight \n2 - View Inventory \n3 - "
+              "(NEED TO LEVEL UP TO PROGRESS) \n4- Add an Apple")
+                #PRINT OPTIONS
 
     selection = input("") #SELECTION DETECTION CODE
     if selection == "1":
@@ -196,7 +199,8 @@ def battleKeep_(Enemy, injured, enemyBufferHp):
     print("")
     print("")
     Enemy.enemyStateChoose_()
-    print("Enemy Health: ", Enemy.health, "    ", "Enemy Power: ", Enemy.power)
+    print("Enemy Health: ", Enemy.health, "    ", 
+          "Enemy Power: ", Enemy.power)
     print("")
     print("Your Health:", health, "    ", "Your Power: ", power)
     print("Choose your action to roll.\n1. Attack\n2. Defend")
@@ -218,7 +222,9 @@ def battleKeep_(Enemy, injured, enemyBufferHp):
             if roll == 1:
                 damageTake = Enemy.power * 2
                 print("You rolled a 1, so the enemy does double damage.")
-                damage = random.randint((int(0.5 * power)),int((0.7 * power)))#SET ATTACK DAMAGE
+                damage = random.randint((int(0.5 * power))\
+                    ,int((0.7 * power)))
+                #SET ATTACK DAMAGE
             elif roll in range(1, 5):
                 damageTake = Enemy.power
                 damage = random.randint(power,int(power * 1.1))
@@ -244,7 +250,8 @@ def battleKeep_(Enemy, injured, enemyBufferHp):
                 time.sleep(1.5)
             if roll == 6:
                 damage = power
-                windowFunction.typeSlowbattle_("You decide to push through the shielding and hit at full damage!")
+                windowFunction.typeSlowbattle_("You decide to push through" 
+                "the shielding and hit at full damage!")
                 time.sleep(1.5)
 
 
@@ -271,7 +278,8 @@ def battleKeep_(Enemy, injured, enemyBufferHp):
             elif roll == 6:
                 if health < 100:
                     health = health + (int(health/10))
-                windowFunction.typeSlowbattle_("You will heal 10% of your health!")
+                windowFunction.typeSlowbattle_(
+                    "You will heal 10% of your health!")
                 time.sleep(0.8)
         elif Enemy.state == "Defend":
             print("Enemy chooses to defend!")
@@ -286,7 +294,8 @@ def battleKeep_(Enemy, injured, enemyBufferHp):
         listHistory.pop(3)
     if listHistory.count("atk") == 3:
         injured = 1
-        windowFunction.typeSlowbattle_("You have been injured! You now take 1.5x damage until the end of the battle!")
+        windowFunction.typeSlowbattle_("You have been injured!"
+        " You now take 1.5x damage until the end of the battle!")
         time.sleep(1.5)
     if injured == 1:
         damageTake = damageTake * 1.5
@@ -367,18 +376,23 @@ def inventory_():
 
 
 def infoView_(name, job, location, age, weight, birthplace, parent1, parent2):
+    #PRINTS OUT THE INFORMATION OF ALLY
     exited = False
     while exited == False:
         selection = input("")
         if selection == "Y":
             windowFunction.ClearWindow_()
             windowFunction.art(7)
-            print(name.upper(), "-", job.upper(), "-", location.upper(), "\n", "AGE:", age.upper(), "\n", "WEIGHT:", weight.upper(), "\n", "BIRTHPLACE:", birthplace.upper(), "\n", "PARENTS:", parent1.upper() + ",", parent2.upper()) 
+            print(name.upper(), "-", job.upper(), "-", location.upper() 
+                , "\n", "AGE:", age.upper(), "\n", "WEIGHT:", weight.upper()
+                , "\n", "BIRTHPLACE:", birthplace.upper()
+                , "\n", "PARENTS:", parent1.upper() + ",", parent2.upper()) 
             exited = True
             input("")
         elif selection == "N":
             exited = True
 
+# MAIN GAME CODE
 windowFunction.ClearFrame_()
 print("Loading", end='\r') #Print loading, do a carriage return
 time.sleep(0.3) #Pause for 0.3 seconds
@@ -392,43 +406,55 @@ windowFunction.BeholdAnimate()
 
 while slopFree == 0:
     slopLength = len(slopList)
-    name = input("                                                       ") #NAME PROMPT
+    name = input("                                  "
+                 "                     ") #NAME PROMPT
     for i in range(slopLength):
         if slopList[i] in name.lower():
             windowFunction.art(2)
-            windowFunction.Text_("BLOODY HELL MATE WHEEEEEEEEEEEEY!!!! COME ON FOOTY LEGENDS!!!!")
+            windowFunction.Text_("BLOODY HELL MATE WHEEEEEEEEEEEEY!!!! "
+                                 "COME ON FOOTY LEGENDS!!!!")
             time.sleep(5)
             exit()
         else:
             slopFree = 1
 
 windowFunction.Center_() #CENTER NAME PROMPT
-windowFunction.typeSlow_("Welcome to a perilous journey. There will be challenges and triumphs. You have seen nothing as of far.") #SLOW TYPE 1 LINE
+windowFunction.typeSlow_("Welcome to a perilous journey. "
+                         "There will be challenges and triumphs. "
+                         "You have seen nothing as of far.") #SLOW TYPE 1 LINE
 time.sleep(1)
-windowFunction.storySetup_("Mladav Bolen - 1654", 0) #SHOW TEXT ON TOP OF IMAGE, PRINT IMAGE BELOW
-windowFunction.Text_("you are surrounded by an air of desolateness.\nA faint rustle breaks the silence for a second.\nIs it time..? I'm probably not going to come out walking but it's worth a try.")
+windowFunction.storySetup_("Mladav Bolen - 1654", 0) 
+#SHOW TEXT ON TOP OF IMAGE, PRINT IMAGE BELOW
+
+windowFunction.Text_("you are surrounded by an air of desolateness.\nA faint "
+                     "rustle breaks the silence for a second.\nIs it time..? "
+                     "I'm probably not going to come out walking but it's "
+                     "worth a try.")#SLOWLY PRINT TEXT + INPUT AT END
 windowFunction.flashAnim_() #CHANGE SCENE WITH FLASHING ANIMATION
-windowFunction.Text_("Oh. What's this then... another one? Bloody hell.. they're like vermin.")
+windowFunction.Text_("Oh. What's this then... another one? "
+                     "Bloody hell.. they're like vermin.")
 
 
-windowFunction.ClearWindow_()
+windowFunction.ClearWindow_()#CLEARS WINDOW FULLY
 windowFunction.storySetup_("Bedroom - Section J", 3)
 windowFunction.Text_("""Your heavy eyes slowly begin to lift open as you peer over to your left.
 A small, slightly stained envelope is sitting next to your bed, laid hastily on your bedstand.
 The letter is enclosed with a bright red wax seal, which seemingly leaked into the package.
 You take a slight whiff and the stench of wax enters your nostrils.
 you begin to sit up and you begin to feel relief that the nightmare was over.
-You take another look at the envelope and see a familiar signature""")
+You take another look at the envelope and see a familiar signature""")#USES RAW STRING
 windowFunction.typeVerySlow_("Signed - Ludvik Novak, with love")
 input()
 
 
 windowFunction.ClearFrame_()
 windowFunction.storySetup_("Bedroom - Section J", 3)
-windowFunction.Text_("The door to your left swings straight open, and a figure stands within the doorway, clenching his arm towards his body.")
+windowFunction.Text_("The door to your left swings straight open, and a "
+                     "figure stands within the doorway, clenching his arm towards his body.")
 print()
-windowFunction.Text_("It's too late.\n\nI'm sorry.\n\nWe were too late.\n\nWhat do we do.. leader?")
-respond_(["WHEEEEEEY"])
+windowFunction.Text_("It's too late.\n\nI'm sorry.\n\nWe were too late."
+                     "\n\nWhat do we do.. leader?")
+respond_()#QUESTION RESPONSE WITH
 windowFunction.Text_("""Look. I may be a military manager but I don't think I can do anything. 
 I don't think us with our thousands of soldiers can make a dent.
 This is your battle.\nThe only one who can fight this fight is you. 
@@ -441,15 +467,21 @@ Thank you.""")
 
 windowFunction.ClearFrame_()
 windowFunction.storySetup_("Bedroom - Section J", 3)
-windowFunction.Text_("You stare at the letter for a bit more, and ultimately decide what must be done.")
+windowFunction.Text_("You stare at the letter for a bit more, "
+                     "and ultimately decide what must be done.")
 
-respond_(["yes", "ok", "okay", "sure", "alright", "yeah", "y", "yea", ""], "Do you want to journey forward?    ")
+respond_(["yes", "ok", "okay", "sure", "alright", "yeah", "y", "yea", ""]
+         , "Do you want to journey forward?    ")#QUESTION RESPONSE
 
 if tick == 1:
-    windowFunction.Text_("Okay okay.. Here's your swordIt's very rusty but it's the best I have currently,\nNow here are some apples to eat on the way.")
+    windowFunction.Text_("Okay okay.. Here's your sword. "
+                         "It's very rusty but it's the best I have currently,"
+                         "\nNow here are some apples to eat on the way.")
     areaLevel_()
 else:
-    windowFunction.Text_("Okay, traitor. Your father would have hoped that you could've avenged him but now I\nSee that is all for naught. It's sad to do,\n\n\n")
+    windowFunction.Text_("Okay, traitor. Your father would have hoped that"
+                         " you could've avenged him but now I\nSee"
+                         " that is all for naught. It's sad to do,\n\n\n")
     windowFunction.typeVerySlow_("But it must be done.")
     exit
 
@@ -484,13 +516,17 @@ You were told the previous night that you could take things but you would rather
 
 NEW ALLY DISCOVERED: BARRY GOLDARMS""")
 print("VIEW INFO ABOUT ALLY? Y/N")
-infoView_("barry goldarms", "master blacksmith", "the old arms", "67", "a lot", "hammerfield", "james somerton", "janet silver")
+infoView_("barry goldarms", "master blacksmith", "the old arms",
+         "67", "a lot", "hammerfield", "james somerton", "janet silver")
 windowFunction.storySetup_("Barry's Metalworks", 5)
-windowFunction.Text_("Oi lad, why 'aven't ya grabbed anythin'?\nI'll just pick out the lightest one in me collection.\nHere, a basic, light blade that's suitable for you")
+windowFunction.Text_("Oi lad, why 'aven't ya grabbed anythin'?\nI'll "
+                     "just pick out the lightest one in me collection."
+                     "\nHere, a basic, light blade that's suitable for you")
 windowFunction.Text_("SWORD OBTAINED!")
 print("SWORD NAME:", CrazyBlade.name)
 CrazyBlade.active = True
-respond_(["yes", "ok", "okay", "sure", "alright", "yeah", "y", "yea", ""], "Would you like to equip this sword?   ")
+respond_(["yes", "ok", "okay", "sure", "alright", "yeah", "y", "yea", ""]
+         , "Would you like to equip this sword?   ")
 if tick == 1:
     equipped = CrazyBlade
     windowFunction.Text_("Sword now equipped")
